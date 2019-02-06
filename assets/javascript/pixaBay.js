32ddb6b32c18d145041da093e6a0c502243ef7dd
DobienC <dobien_c@yahoo.com>
2019-02-05 20:42:19 -0800

    Video Works

--- assets/javascript/pixaBay.js
+++ assets/javascript/pixaBay.js
@@ -1,6 +1,7 @@
 
 var key = "11482244-d75f6775f0b5a86ee7680f6b7";
-var search_query = "&q=" + "dog";
+var queryInput = "puppies"
+var search_query = "&q=" + queryInput;
 var queryURL = "https://pixabay.com/api/videos/?key=" + key + search_query;
 
 
@@ -8,5 +9,13 @@ $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response){
-    console.log(response);
+    var $video = $("<video autoplay loop>");
+    $video.attr("width", 400);
+    $video.attr("height", 480);
+    var $source = $("<source>");
+    $source.attr("src", response.hits[0].videos.large.url);
+    $source.attr("type", "video/mp4");
+    $video.append($source);
+    console.log(response.hits[0].videos.large.url);
+    $("#pixaVid").append($video);
 });
