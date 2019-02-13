004e2fbc82a31d59c689975f3bf3643a92a14437
Volodymyr Kovalenko <vokovalenko16@gmail.com>
2019-02-12 18:37:22 -0800

    favorites added

--- Sounds/assets/javascript/script.js
+++ Sounds/assets/javascript/script.js
@@ -39,6 +39,8 @@ var item = $("<div>").addClass("item")
   favorites.addClass("favbutton")
   favorites.text("Add.")
   favorites.attr("url", element.previewURL)
+  favorites.attr("img", element.image)
+  favorites.attr("name", element.name)
 
 
   var newPic = $("<img>").attr("src", element.image);
@@ -60,8 +62,46 @@ var item = $("<div>").addClass("item")
       });
     });
   });
-  
-    $(document).on("click", ".favbutton", function() {
-      console.log($(this).attr("url"))
+
+  var database = firebase.database();
+
+  let favSONG = {
+    url: ($(this).attr("url")),
+    img: ($(this).attr("img")),
+    name: ($(this).attr("name"))
+  };
+      // Initial Values
+      var url = '';
+      var img = '';
+      var name = '';
+
+      // Capture Button Click
+      $(document).on("click", ".favbutton", function() {
+        event.preventDefault();
+
+        // Grabbed values from text-boxes
+        url = ($(this).attr("url")),
+        name = $(this).attr("name"),
+        img =  $(this).attr("img")
+    
+        // Code for "Setting values in the database"
+      var newFav = {
+          url: url,
+          name: name,
+          img: img,
+        };
+      // Firebase watcher + initial loader HINT: .on("value")
+      database.ref().push(newFav) 
+        'value',
+        function(snapshot) {
+          // Log everything that's coming out of snapshot
+          console.log(snapshot.val());
+
+      $("#url").val("")
+      $("#name").val("")
+      $("#img").val("")
+
+    };
   });
 });
+
